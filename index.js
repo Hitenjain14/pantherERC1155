2c47823ab174d2f4b98b0eaeecabfeb4c471b5dd
Hiten Jain <hitenjain146@gmail.com>
2022-06-17 00:59:51 +0530

    changes

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const moralisUrl =
   'https://speedy-nodes-nyc.moralis.io/39f9e12bf020c85e5091c894/polygon/mumbai';
 
 const init = async () => {
-  try {
-    const provider = new Provider(privateKey, moralisUrl);
-    // @ts-ignore
-    const web3 = new Web3(provider);
-    const contract = new web3.eth.Contract(abi, address);
-    for (let i = 0; i < 10; i++) {
+  const provider = new Provider(privateKey, moralisUrl);
+  // @ts-ignore
+  const web3 = new Web3(provider);
+  const contract = new web3.eth.Contract(abi, address, { gas: 1000000 });
+  for (let i = 0; i < 10; i++) {
+    try {
       console.log(`minting ${i} token`);
       const txn = await contract.methods.mintOne('', 1, 1).send({ from: '' });
       console.log(txn);
+    } catch (err) {
+      console.log(`error in minting ${i} token`);
     }
-  } catch (err) {
-    console.log(err.message);
   }
 };
 init();
