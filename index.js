94a97c9eb295c9e688f05b4b07799814f1050917
Hiten Jain <hitenjain146@gmail.com>
2022-06-17 14:41:16 +0530

    mainnet

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Web3 = require('web3');
 const Provider = require('@truffle/hdwallet-provider');
 const { abi } = require('./contract');
-const address = '';
+const address = '0x79BAAbA8eF2F4661306e5e31bBf6cA87686c68be';
 const privateKey = '';
 const moralisUrl =
   'https://speedy-nodes-nyc.moralis.io/e9251bb6176f4c0d110be505/polygon/mainnet';
@@ -13,7 +13,7 @@ const init = async () => {
   // @ts-ignore
   const web3 = new Web3(provider);
 
-  const contract = new web3.eth.Contract(abi, address, { gas: 80000 });
+  const contract = new web3.eth.Contract(abi, address, { gas: 70000 });
   for (let i = 0; i < list.length; i++) {
     const gasPrice = await web3.eth.getGasPrice();
     const gwei = gasPrice / 1000000000;
@@ -26,7 +26,7 @@ const init = async () => {
       console.log(`minting ${i} token`);
       const txn = await contract.methods
         .mintOne(list[i].wallets, 1, 1)
-        .send({ from: '' });
+        .send({ from: '0x59109E77C2d9eE837768425C4f37e6BdfF2cFaD1' });
       console.log(txn);
     } catch (err) {
       console.log(err.message);
