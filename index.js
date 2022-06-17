a059cd587047c888c9a74ce1072afe1b08f30354
Hiten Jain <hitenjain146@gmail.com>
2022-06-17 15:53:20 +0530

    added logs

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const address = '0x79BAAbA8eF2F4661306e5e31bBf6cA87686c68be';
 const privateKey = '';
 const moralisUrl =
   'https://polygon-mainnet.g.alchemy.com/v2/8lVyeqfXiotIGQRtXNlooUjmEjJDvg7y';
+const fs = require('fs');
 
 const list = require('./pantherWhitelist.json');
 
@@ -14,24 +15,29 @@ const init = async () => {
   const web3 = new Web3(provider);
 
   const contract = new web3.eth.Contract(abi, address, { gas: 80000 });
-  for (let i = 5; i < list.length; i++) {
+  for (let i = 6; i < list.length; i++) {
     const gasPrice = await web3.eth.getGasPrice();
     const gwei = gasPrice / 1000000000;
-    // console.log(gwei);
-    if (gwei > 400) {
+    if (gwei <= 400) {
       console.log('skipped', list[i].wallets);
-      continue;
-    }
-    try {
-      console.log(`minting ${i} token`);
-      const txn = await contract.methods.mintOne(list[i].wallets, 1, 1).send({
-        from: '0x59109E77C2d9eE837768425C4f37e6BdfF2cFaD1',
-        gasPrice: gasPrice,
-      });
-      console.log(txn);
-    } catch (err) {
-      console.log(err);
-      console.log(`error in minting ${i} token`);
+      const data = fs.readFileSync('missed.json');
+      const json = JSON.parse(data.toString());
+      console.log(json);
+      json['wallets'].push({ wallet: list[i].wallets.toString() });
+      let jsonStr = JSON.stringify(json);
+      fs.writeFile('missed.json', jsonStr, (err) => {});
+
+      try {
+        console.log(`minting ${i} token`);
+        const txn = await contract.methods.mintOne(list[i].wallets, 1, 1).send({
+          from: '0x59109E77C2d9eE837768425C4f37e6BdfF2cFaD1',
+          gasPrice: gasPrice,
+        });
+        console.log(txn);
+      } catch (err) {
+        console.log(err);
+        console.log(`error in minting ${i} token`);
+      }
     }
   }
 };
