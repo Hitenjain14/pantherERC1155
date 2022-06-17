ec96dc25e5f2d1103113d85ce9fb50ed30469387
Hiten Jain <hitenjain146@gmail.com>
2022-06-17 15:31:13 +0530

    new node

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { abi } = require('./contract');
 const address = '0x79BAAbA8eF2F4661306e5e31bBf6cA87686c68be';
 const privateKey = '';
 const moralisUrl =
-  'https://speedy-nodes-nyc.moralis.io/e9251bb6176f4c0d110be505/polygon/mainnet';
+  'https://polygon-mainnet.g.alchemy.com/v2/8lVyeqfXiotIGQRtXNlooUjmEjJDvg7y';
 
 const list = require('./pantherWhitelist.json');
 
@@ -13,23 +13,24 @@ const init = async () => {
   // @ts-ignore
   const web3 = new Web3(provider);
 
-  const contract = new web3.eth.Contract(abi, address, { gas: 70000 });
-  for (let i = 0; i < list.length; i++) {
+  const contract = new web3.eth.Contract(abi, address, { gas: 80000 });
+  for (let i = 5; i < list.length; i++) {
     const gasPrice = await web3.eth.getGasPrice();
     const gwei = gasPrice / 1000000000;
-    console.log(gwei);
-    if (gwei > 200) {
+    // console.log(gwei);
+    if (gwei > 400) {
       console.log('skipped', list[i].wallets);
       continue;
     }
     try {
       console.log(`minting ${i} token`);
-      const txn = await contract.methods
-        .mintOne(list[i].wallets, 1, 1)
-        .send({ from: '0x59109E77C2d9eE837768425C4f37e6BdfF2cFaD1' });
+      const txn = await contract.methods.mintOne(list[i].wallets, 1, 1).send({
+        from: '0x59109E77C2d9eE837768425C4f37e6BdfF2cFaD1',
+        gasPrice: gasPrice,
+      });
       console.log(txn);
     } catch (err) {
-      console.log(err.message);
+      console.log(err);
       console.log(`error in minting ${i} token`);
     }
   }
